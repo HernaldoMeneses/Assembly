85b91af5a22218518ba0fa768b5ab09634b76574
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-11 08:57:13 -0300

    Create 1-Why-Learn-this.asm

---
+++ 1-Why-Learn-this.asm
@@ -0,0 +1,16 @@
+;Amazing!
+;   You're actually reading this.
+;   you are by: (forced | accidente | interest )
+;   Assembly What's: ( right | wrong )                 #- very important
+;   Resons to read this:
+    ;   course
+    ;   maintain program code in Assembly
+    ;   your was forced writen in Assembly
+    ;   you need speed in your program
+    ;   you want to undertand how computer relly works
+    ;   you want learning how to write efficient code
+    ;   try something new
+
+;Let's go...
+;
+;file:///L:/Linear_/Base-Jump_02/Art%20Of%20Intel%20x86%20Assembly.pdf
