193bc10943d475dc41a550100bcd28d6a45fe888
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-05-02 15:29:53 -0300

    Created 1.1.3-Binary-Formats.asm

---
+++ 1-The-Art-of-Assembly/1.1.3-Binary-Formats.asm
@@ -0,0 +1,20 @@
+;Any number of leading zero bits may precede the binary
+;number without changing its value.
+;ex 101 = 5 as all others
+0000000000101
+000000000101
+00000000101
+0000000101
+000000101
+00000101
+0000101
+000101
+00101
+0101
+101
+
+;To facility reading
+000000000101
+0000 0000 0101
+
+;pg35
